89f236fa12fc09f2f11aaf877b2d1fd195b97910
MelinaVillalba <126410186+MelinaVillalba@users.noreply.github.com>
2024-06-01 10:12:36 -0300

    Create Comida.cpp

---
+++ Comida.cpp
@@ -0,0 +1,31 @@
+#include "Comida.h"
+#include <stdlib.h>
+
+Comida::Comida()
+{
+    _texture.loadFromFile("Assets/apple.bmp");
+
+    _sprite.setTexture(_texture);
+
+    _sprite.setOrigin(_sprite.getGlobalBounds().width/2, _sprite.getGlobalBounds().height/2);
+
+}
+
+ sf::FloatRect Comida::getBounds()const
+{
+    return _sprite.getGlobalBounds();
+}
+
+void Comida::draw(sf::RenderTarget& target, sf::RenderStates states)const
+{
+    target.draw(_sprite, states);
+}
+
+void Comida::respawn()
+{
+    _sprite.setPosition(std::rand()%700 + _sprite.getGlobalBounds().width, std::rand()%500 + _sprite.getGlobalBounds().height);
+}
+Comida::~Comida()
+{
+    //dtor
+}
