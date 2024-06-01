c08f82d4386bf0fdf58738a5115bd3a8cdcf5f0b
MelinaVillalba <126410186+MelinaVillalba@users.noreply.github.com>
2024-06-01 10:11:51 -0300

    Create personaje.cpp

---
+++ personaje.cpp
@@ -0,0 +1,213 @@
+#include "personaje.h"
+#include <SFML/Graphics.hpp>
+#include <iostream> // Funciones de salida
+#include <conio.h> // Tiempos
+#include <stdlib.h>
+#include <windows.h>
+
+personaje::personaje() : currentDirection(Direction::None)
+{
+    _texture.loadFromFile("Assets/cabeza.bmp");
+
+    _sprite.setTexture(_texture);
+    //posicionamos a la serpiente en medio de la pantalla
+    _sprite.setPosition(400, 300);
+//    _sprite.setOrigin(_sprite.getGlobalBounds().width/2,  _sprite.getGlobalBounds().height);
+}
+
+void personaje::update()
+{
+
+//Movimiento del personaje
+
+    //Guardamos el valor de la tecla presionada
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            //Si snake va hacia abajo no puede ir hacia arriba
+            if(currentDirection != Direction::Down)
+                {
+                    currentDirection = Direction::Up;
+                }
+
+        }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            //Si snake va hacia  arriba no puede ir hacia abajo
+            if (currentDirection !=Direction::Up)
+                {
+                    currentDirection = Direction::Down;
+                }
+
+        }
+
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            //Si snake va hacia la derecha no puede ir hacia la izquierda
+            if(currentDirection !=Direction::Right)
+                {
+                    currentDirection = Direction::Left;
+                }
+
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                //Si snake va hacia la izquierda no puede ir hacia la derecha
+                if(currentDirection !=Direction::Left)
+                    {
+                        currentDirection = Direction::Right;
+                    }
+
+            }
+
+
+  // Snake se mueve en base a currentDirection
+    switch (currentDirection)
+        {
+            case Direction::Up:
+                _sprite.move(0, -_velocidad);
+                break;
+            case Direction::Down:
+                _sprite.move(0, _velocidad);
+                break;
+            case Direction::Left:
+                _sprite.move(-_velocidad, 0);
+                break;
+            case Direction::Right:
+                _sprite.move(_velocidad, 0);
+                break;
+            default:
+                break;
+        }
+
+
+        //La imagen se orienta segun la direccion que se tome
+        if (currentDirection==Direction::Left)
+            {
+                    _sprite.setScale(-1, 1);
+            }
+          else if (currentDirection==Direction::Right)
+            {
+                    _sprite.setScale(1, 1);
+            }
+
+          else if (currentDirection==Direction::Up)
+            {
+                    _sprite.setScale(1, -1);
+            }
+
+            else if (currentDirection==Direction::Down)
+            {
+                    _sprite.setScale(1, 1);
+            }
+
+
+
+        //limites LEFT del personaje
+       if(_sprite.getPosition().x<65)
+            {
+                _sprite.setPosition(65, _sprite.getPosition().y);
+            }
+        //limites UP del personaje
+        if(_sprite.getPosition().y<65)
+            {
+                _sprite.setPosition( _sprite.getPosition().x,65);
+            }
+        //limites  RIGHT del personaje
+        if(_sprite.getPosition().x>740)
+            {
+               _sprite.setPosition(740, _sprite.getPosition().y);
+            }
+       //limites  DOWN del personaje
+       if(_sprite.getPosition().y>540)
+            {
+                _sprite.setPosition(_sprite.getPosition().x, 540);
+            }
+
+            // si choca con abajo
+        if(_sprite.getPosition().y==540)
+            {
+                 _sprite.setPosition(400, 300);
+                 currentDirection= Direction::None;
+                    if (life > 0 )
+                        {
+                            life--;
+                            cout << life << endl;
+                        }
+                        if(life ==0 && life > -1)
+                            {
+                                cout << "Game Over"<<endl;
+                            }
+            }
+
+            // si choca con arriba
+            if(_sprite.getPosition().y==65)
+                {
+                     _sprite.setPosition(400, 300);
+                     currentDirection= Direction::None;
+                        if (life > 0 )
+                            {
+                                life--;
+                                cout << life << endl;
+                            }
+                            if(life ==0 && life > -1)
+                                {
+                                    cout << "Game Over"<<endl;
+                                }
+                }
+
+            //si choca con izquierda
+             if(_sprite.getPosition().x==65)
+                {
+                     _sprite.setPosition(400, 300);
+                     currentDirection= Direction::None;
+                        if (life > 0 )
+                            {
+                                life--;
+                                cout << life << endl;
+                            }
+                            if(life ==0 && life > -1)
+                                {
+                                    cout << "Game Over"<<endl;
+                                }
+                }
+
+                //si choca con derecha
+                if(_sprite.getPosition().x==740)
+                    {
+                         _sprite.setPosition(400, 300);
+                         currentDirection= Direction::None;
+                            if (life > 0 )
+                                {
+                                    life--;
+                                    cout << life << endl;
+                                }
+                                if(life ==0 && life > -1)
+                                    {
+                                        cout << "Game Over"<<endl;
+                                    }
+                    }
+
+}
+
+ sf::FloatRect personaje::getBounds() const
+ {
+     return _sprite.getGlobalBounds();
+ }
+
+void personaje::aumentoVelocidad()
+{
+    //_velocidad +20;
+}
+//const porque no modifica el estado actual del personaje
+void personaje::draw(sf::RenderTarget& target, sf:: RenderStates states) const
+{
+    //como se va a dibujar el personaje
+    target.draw(_sprite, states);
+}
+
+
+
+personaje::~personaje()
+{
+    //dtor
+}
